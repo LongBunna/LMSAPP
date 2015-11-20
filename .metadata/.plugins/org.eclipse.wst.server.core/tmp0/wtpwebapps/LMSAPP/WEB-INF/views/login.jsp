623f4bd0e4098f4b73b1c1e3078f5fac4c45f81b
chitrasem <sabaychitra84@gmail.com>
2015-11-20 10:30:22 +0700

    Make some change from Chitra

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/LMSAPP/WEB-INF/views/login.jsp
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/LMSAPP/WEB-INF/views/login.jsp
@@ -1,11 +1,11 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ page isELIgnored="false" %>
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 		<title>Login page</title>
-		<link href="<c:url value='/static/bootstrap-3.3.5-dist/css/bootstrap.css' />"  rel="stylesheet"></link>
+		<link href="<c:url value='/static/css/bootstrap.css' />"  rel="stylesheet"></link>
 		<link href="<c:url value='/static/css/app.css' />" rel="stylesheet"></link>
 		<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.css" />
 	</head>
--- LMSAPP/src/main/java/com/kosign/lmsapp/configuration/SecurityConfiguration.java
+++ LMSAPP/src/main/java/com/kosign/lmsapp/configuration/SecurityConfiguration.java
@@ -10,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	CustomSuccessHandler customSuccessHandler;
  
     @Autowired
     public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
@@ -26,7 +29,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .antMatchers("/", "/home").permitAll() 
         .antMatchers("/admin/**").access("hasRole('ADMIN')")
         .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
-        .and().formLogin().loginPage("/login")
+        .and().formLogin().loginPage("/login").successHandler(customSuccessHandler)
         .usernameParameter("ssoId").passwordParameter("password")
         .and().csrf()
         .and().exceptionHandling().accessDeniedPage("/Access_Denied");
